a9915389e620126dd972b6eac24e5e2fd96d2e78
saraheliz7 <woodsose@gmail.com>
2018-02-09 13:17:39 -0500

    Tested earnings from the different rates for before bed, before midnight, and after midnight

--- src/babysitter.js
+++ src/babysitter.js
@@ -1,8 +1,10 @@
 const convertTime = (timeString) => {
     let time = timeString.slice(0, -2);
 
-    if(timeString.includes("am")) {
-        return Number(time) + 100;
+    if(timeString.includes("am") && timeString.includes("12")) {
+        return (Number(time) + 12) * 100;
+    } else if(timeString.includes("am")) {
+        return Number(time) * 100;
     } else if(timeString.includes("pm")) {
         return (Number(time) + 12) * 100;
     }
@@ -15,13 +17,29 @@ const babysitter = (startTime, bedTime, endTime) => {
     let convertedBed = convertTime(bedTime);
     let convertedEnd = convertTime(endTime);
 
-    let startPay = ((convertedBed - convertedStart) / 100) * 12;
-    let bedPay = 0;
-    let endPay = 0;
-    let finalPay = startPay + bedPay + endPay;
+    let firstPay = 12 * ((convertedBed - convertedStart) / 100);
+    let secondPay = 8 * ((2400 - convertedBed)/ 100);
+    let thirdPay = 16 * (convertedEnd / 100);
+
+    if(convertedBed === convertedEnd) {
+        secondPay = 0;
+        thirdPay = 0;
+    }
+
+    if(convertedStart >= convertedBed) {
+        firstPay = 0;
+        thirdPay = 0;
+    }
+
+    if(convertedStart === 2400) {
+        firstPay = 0;
+        secondPay = 0;
+        thirdPay = 16 * (convertedEnd / 100);
+    }
 
 
 
-return finalPay;
+
+    return firstPay + secondPay + thirdPay;
 };
 module.exports = babysitter;
--- test/babysitter.spec.js
+++ test/babysitter.spec.js
@@ -2,7 +2,16 @@ const expect = require("chai").expect;
 const babysitter = require("../src/babysitter");
 
 describe("The babysitter", function() {
+
     it("gets paid $12 an hour when babysitting before bedtime", function() {
         expect(babysitter("5pm", "7pm", "7pm")).to.equal(24);
+    });
+
+    it("gets paid $8 an hour when babysitting between bed time and midnight", function() {
+        expect(babysitter("7pm", "7pm", "12am")).to.equal(40);
+    });
+
+    it("gets paid $16 an hour when babysitting after midnight", function() {
+        expect(babysitter("12am", "12am", "4am")).to.equal(64)
     });
 });
